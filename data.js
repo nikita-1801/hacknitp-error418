016896cfc85ce8fa3d87a9de61fbffbfb0f6f4fe
devanshi-katyal <60283765+devanshi-katyal@users.noreply.github.com>
2021-01-16 13:16:40 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,38 @@
+const data=[
+    {
+        product: "Bags",
+        subProducts: [
+            {
+                name: "Jute bag",
+                link: "https://www.youtube.com/",
+                fact:"why you should buy 1"
+            },
+            {
+                name: "cloth bag",
+                link: "https://www.youtube.com/",
+                fact:"why you should buy 2"
+            }
+        ]
+    },
+    {
+        product: "bottles",
+        subProducts: [
+            {
+                name: "plastic bottles",
+                link: "https://www.youtube.com/",
+                fact:"why you should buy 3"
+            },
+            {
+                name: "glass bottles",
+                link: "https://www.youtube.com/",
+                fact:"why you should buy 4"
+            },
+            {
+                name: "paper bottle packs",
+                link: "https://www.youtube.com/",
+                fact:"why you should buy 5"
+            }
+        ]
+    }
+];
+export default data;
